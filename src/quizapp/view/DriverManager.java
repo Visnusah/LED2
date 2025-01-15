737e1d1a3cb7b9309ca3b01408e69501a18c2bf2
kamlesh sah <Sahk0292@gmail.com>
2025-01-15 15:39:52 +0545

    Feat: Add signup functionality

---
+++ src/quizapp/view/DriverManager.java
@@ -0,0 +1,12 @@
+
+package quizapp.view;
+
+import com.sun.jdi.connect.spi.Connection;
+
+class DriverManager {
+
+    static Connection getConnection(String jdbcmysqllocalhost3306quizapp, String root, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/quizapp/view/PreparedStatement.java
@@ -0,0 +1,14 @@
+
+package quizapp.view;
+
+class PreparedStatement {
+
+    void setString(int i, String username) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    int executeUpdate() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
